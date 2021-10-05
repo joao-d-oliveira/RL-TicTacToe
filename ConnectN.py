d8fed008daef3a5d9b31e418a28ff70a17fce9db
Joao Oliveira <joao.d.oliveira@gmail.com>
2021-10-05 19:34:26 -0300

    adding Play class to game

--- ConnectN.py
+++ ConnectN.py
@@ -1,9 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+import numpy as np
 from copy import copy
 
 
@@ -16,6 +13,7 @@ def get_runs(v, i):
      ends, = np.where(difs < 0)
      return starts, ends, ends-starts
 
+
 # see if vector contains N of certain number in a row
 def in_a_row(v, N, i):
      if len(v) < N:
@@ -25,7 +23,6 @@ def in_a_row(v, N, i):
           return np.any(total >= N) 
      
 
- 
 def get_lines(matrix, loc):
 
      i,j=loc
@@ -64,10 +61,6 @@ def get_lines(matrix, loc):
      diag_left = np.concatenate([flat[tr:pos:h-1],flat[pos:bl+1:h-1]])
 
      return hor, ver, diag_right, diag_left
-        
-
-
-
 
 
 class ConnectN:
@@ -222,3 +215,142 @@ class ConnectN:
 
      def available_mask(self):
           return (np.abs(self.state) != 1).astype(np.uint8)
+
+
+class Play:
+
+     def __init__(self, game, player1=None, player2=None, policy1=None, policy2=None, name='game'):
+          self.original_game = game
+          self.game = copy(game)
+          self.player1 = player1
+          self.policy1 = policy1
+          self.player2 = player2
+          self.policy2 = policy2
+          self.player = self.game.player
+          self.end = False
+          self.play()
+
+     def reset(self):
+          self.game = copy(self.original_game)
+          self.click_cid = None
+          self.end = False
+
+     def play(self, name='Game'):
+
+          self.reset()
+
+          if self.game.w * self.game.h < 25:
+               figsize = (self.game.w / 1.6, self.game.h / 1.6)
+
+          else:
+               figsize = (self.game.w / 2.1, self.game.h / 2.1)
+
+          self.fig = plt.figure(name, figsize=figsize)
+          if self.game.w * self.game.h < 25:
+               self.fig.subplots_adjust(.2, .2, 1, 1)
+          else:
+               self.fig.subplots_adjust(.1, .1, 1, 1)
+
+          self.fig.show()
+          w, h = self.game.size
+          self.ax = self.fig.gca()
+          self.ax.grid()
+          # remove hovering coordinate tooltips
+          self.ax.format_coord = lambda x, y: ''
+          self.ax.set_xlim([-.5, w - .5])
+          self.ax.set_ylim([-.5, h - .5])
+          self.ax.set_xticks(np.arange(0, w, 1))
+          self.ax.set_yticks(np.arange(0, h, 1))
+          self.ax.set_aspect('equal')
+
+          for loc in ['top', 'right', 'bottom', 'left']:
+               self.ax.spines[loc].set_visible(False)
+
+          # fully AI game
+          if self.player1 is not None and self.player2 is not None:
+               self.anim = FuncAnimation(self.fig, self.draw_move, frames=self.move_generator, interval=500,
+                                         repeat=False)
+               return
+
+          # at least one human
+          if self.player1 is not None:
+               # first move from AI first
+               succeed = False
+               while not succeed:
+                    loc = self.player1(self.game, self.policy1) if self.policy1 is not None else self.player1(self.game)
+                    succeed = self.game.move(loc)
+
+               self.draw_move(loc)
+
+          self.click_cid = self.fig.canvas.mpl_connect('button_press_event', self.click)
+
+     def move_generator(self):
+          score = None
+          # game not concluded yet
+          while score is None:
+               self.player = self.game.player
+               if self.game.player == 1:
+                    loc = self.player1(self.game, self.policy1) if self.policy1 is not None else self.player1(self.game)
+               else:
+                    loc = self.player2(self.game, self.policy2) if self.policy2 is not None else self.player2(self.game)
+
+               success = self.game.move(loc)
+
+               # see if game is done
+               if success:
+                    score = self.game.score
+                    yield loc
+
+     def draw_move(self, move=None):
+          if self.end:
+               return
+
+          i, j = self.game.last_move if move is None else move
+          c = 'salmon' if self.player == 1 else 'lightskyblue'
+          self.ax.scatter(i, j, s=500, marker='o', zorder=3, c=c)
+          score = self.game.score
+          self.draw_winner(score)
+          self.fig.canvas.draw()
+
+     def draw_winner(self, score):
+          if score is None:
+               return
+
+          if score == -1 or score == 1:
+               locs = self.game.get_winning_loc()
+               c = 'darkred' if score == 1 else 'darkblue'
+               self.ax.scatter(locs[:, 0], locs[:, 1], s=300, marker='*', c=c, zorder=4)
+
+          # try to disconnect if game is over
+          if hasattr(self, 'click_cid'):
+               self.fig.canvas.mpl_disconnect(self.click_cid)
+
+          self.end = True
+
+     def click(self, event):
+
+          loc = (int(round(event.xdata)), int(round(event.ydata)))
+          self.player = self.game.player
+          succeed = self.game.move(loc)
+
+          if succeed:
+               self.draw_move()
+
+          else:
+               return
+
+          if self.player1 is not None or self.player2 is not None:
+
+               succeed = False
+               self.player = self.game.player
+               while not succeed:
+                    if self.game.player == 1:
+                         loc = self.player1(self.game, self.policy1) if self.policy1 is not None else self.player1(
+                              self.game)
+                    else:
+                         loc = self.player2(self.game, self.policy2) if self.policy2 is not None else self.player2(
+                              self.game)
+
+                    succeed = self.game.move(loc)
+
+               self.draw_move()
